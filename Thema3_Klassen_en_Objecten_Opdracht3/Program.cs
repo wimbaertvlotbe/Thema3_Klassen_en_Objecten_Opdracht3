a66ec6e982208fc9b31481c8c329c9c317ddbf54
wimbaertvlotbe <36698691+wimbaertvlotbe@users.noreply.github.com>
2021-09-20 11:43:24 +0200

    Add project files.

---
+++ Thema3_Klassen_en_Objecten_Opdracht3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Thema3_Klassen_en_Objecten_Opdracht3
+{
+    /*
+        Wanneer je OOP toepast dan komt alle code in objecten te staan.
+        Die objecten  bevatten attributen en methods.De attributen en methods 
+        bepalen het gedrag van objecten.
+
+
+        In opdracht 1 en 2 heb je de Klasse Balk gemaakt en voorzien van de attributen 
+        lengte, breedte en hoogte.Die drie attributen beschrijven de dimensies van de balk.
+        Ze horen dus in een afzonderlijke klasse te zitten. Elke dimensie heeft dan een 
+        naam (vb.lengte, breedte), eenheid (vb.m, mm) en waarde (vb. 3.22). 
+
+        OPDRACHT:
+
+        1. Programmeer de klasse Dimensie aan.Dit is een klasse die één ruimtedimensie bevat.
+           Een dimensie heeft een een naam , eenheid en waarde.
+        2. Programmeer een klasse Afmetingen aan.Deze klasse bevat de dimensies van de balk. 
+           Een balk heeft drie ruimte-dimensies (lengte, breedte, hoogte).
+        3. Vervang in de klasse Balk de attributen lengte, breedte en hoogte door
+           een attribuut van de klasse Afmetingen. Instantieer een object van de klasse 
+           Balk aan en geef waarden aan het attribuut afmetingen.
+
+    */
+
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
